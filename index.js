c4a3a282397c95c5f21c6a4667834c1e63184806
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-08-03 15:24:23 +0200

    feat: parse request body

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -17,6 +16,11 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', express.urlencoded(), function(req, res) {
+    console.log(req.body);
+    res.json({original_url: req.body.url});
 });
 
 app.listen(port, function() {
