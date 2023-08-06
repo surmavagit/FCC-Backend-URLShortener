3036202a61aae0c641e636bd36206751b3ca2b5f
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-08-06 16:06:31 +0200

    refactor: split in two middleware functions; autoformat file

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const dns = require('node:dns');
+const dns = require("node:dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.post('/api/shorturl', express.urlencoded(), function(req, res) {
-    const invalid = {error: 'Invalid URL'};
-    const url = req.body.url;
-    let prurl = '';
-    const http = 'http://';
-    const https = 'https://';
-    if (url.startsWith(https)) {
-        prurl = url.slice(https.length);
-    } else if (url.startsWith(http)) {
-        prurl = url.slice(http.length);
-    }
+app.post("/api/shorturl", express.urlencoded(), parseUrl, dnsLookup);
 
-    let host = '';
-    if (prurl) {
-        const pathIndex = prurl.indexOf('/');
-        if (pathIndex != -1) {
-            host = prurl.slice(0, prurl.indexOf('/'));
-        } else {
-            host = prurl;
-        }
-    }
-
-    if (host) {
-        dns.lookup(host, function(err, address, family) {
-            if (err) {
-                res.json(invalid);
-            } else {
-                res.json({original_url: url});
-            }
-        });
-    } else {
-        res.json(invalid);
-    }
-});
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+async function parseUrl(req, res, next) {
+  const invalid = { error: "Invalid URL" };
+  const url = req.body.url;
+  const http = "http://";
+  const https = "https://";
+  if (!url.startsWith(http) && !url.startsWith(https)) {
+    res.json(invalid);
+  } else {
+    let prurl = "";
+    if (url.startsWith(https)) {
+      prurl = url.slice(https.length);
+    } else {
+      prurl = url.slice(http.length);
+    }
+    if (!prurl) {
+      res.json(invalid);
+    } else {
+      let host = "";
+      const pathStartsAt = prurl.indexOf("/");
+      if (pathStartsAt != -1) {
+        host = prurl.slice(0, prurl.indexOf("/"));
+      } else {
+        host = prurl;
+      }
+      if (!host) {
+        res.json(invalid);
+      } else {
+        req.data = host;
+        next();
+      }
+    }
+  }
+}
+
+async function dnsLookup(req, res) {
+  if (host) {
+    dns.lookup(host, function (err, address, family) {
+      if (err) {
+        res.json(invalid);
+      } else {
+        res.json({ original_url: url });
+      }
+    });
+  } else {
+    res.json(invalid);
+  }
+}
